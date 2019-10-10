497056084ba166c3bde941eb3d1af7b6af05c66b
sumgr0 <sumit.grover@gmail.com>
2019-10-10 14:37:02 +0530

    update

--- rapid_seven.sh
+++ rapid_seven.sh
@@ -3,12 +3,13 @@
 api=""
 year=$(date +'%Y')
 month=$(date +'%m')
-fdns=$(curl -H "X-Api-Key: $api" "https://us.api.insight.rapid7.com/opendata/studies/sonar.fdns_v2/" --silent | jq -r '.sonarfile_set[]' | grep -m 1 -E "$year-$month-[0-3]{1}[0-9]{1}-[0-9]{10}-fdns_any.json.gz")
-cname=$(curl -H "X-Api-Key: $api" "https://us.api.insight.rapid7.com/opendata/studies/sonar.fdns_v2/" --silent | jq -r '.sonarfile_set[]' | grep -m 1 -E "$year-$month-[0-3]{1}[0-9]{1}-[0-9]{10}-fdns_cname.json.gz")
-f_link=$(curl -H "X-Api-Key: $api" "https://us.api.insight.rapid7.com/opendata/studies/sonar.fdns_v2/$fdns/download/" --silent | jq '.url' | sed 's/\"//g')
-c_link=$(curl -H "X-Api-Key: $api" "https://us.api.insight.rapid7.com/opendata/studies/sonar.fdns_v2/$cname/download/" --silent | jq '.url' | sed 's/\"//g')
 
+fdns=$(curl -H "X-Api-Key: $api" "https://us.api.insight.rapid7.com/opendata/studies/sonar.fdns_v2/" --silent | jq -r '.sonarfile_set[]' | grep -m 1 -E "$year-$month-[0-3]{1}[0-9]{1}-[0-9]{10}-fdns_any.json.gz")
+f_link=$(curl -H "X-Api-Key: $api" "https://us.api.insight.rapid7.com/opendata/studies/sonar.fdns_v2/$fdns/download/" --silent | jq '.url' | sed 's/\"//g')
 wget -bqc "$f_link" -O ~/recon/data/fdns_any.json.gz
+
+cname=$(curl -H "X-Api-Key: $api" "https://us.api.insight.rapid7.com/opendata/studies/sonar.fdns_v2/" --silent | jq -r '.sonarfile_set[]' | grep -m 1 -E "$year-$month-[0-3]{1}[0-9]{1}-[0-9]{10}-fdns_cname.json.gz")
+c_link=$(curl -H "X-Api-Key: $api" "https://us.api.insight.rapid7.com/opendata/studies/sonar.fdns_v2/$cname/download/" --silent | jq '.url' | sed 's/\"//g')
 wget -bqc "$c_link" -O ~/recon/data/fdns_cname.json.gz
 
 file=$(curl -H "X-Api-Key: $api" "https://us.api.insight.rapid7.com/opendata/studies/sonar.rdns_v2/" --silent | jq -r '.sonarfile_set[0]')
